0e5f98c561b307e2ced6946609646fc84b835ccd
kimhyunkwang <khkhy8712@gmail.com>
2021-04-07 20:11:11 +0900

    Update : connection with mysql database

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,19 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import pymysql
+import config
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(config)
+
+    # ORM
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     # 블루프린트
     from .views import index_views, residence_views, hospital_views
