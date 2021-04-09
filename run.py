2cd2c896de7c0996811df940b8dae06a61b9a90d
kkl <ycdi2223@naver.com>
2021-04-09 16:28:20 +0900

    docker-compose 파일  추가

--- run.py
+++ run.py
@@ -2,5 +2,9 @@ from app import create_app
 
 app = create_app()
 
+@app.route('/', methods=['GET'])
+def index():
+        return 'Hello World!'
+        
 if __name__ == '__main__':
     app.run('localhost', port=5000, debug=True)
