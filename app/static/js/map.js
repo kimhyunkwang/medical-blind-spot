54ef1d2ce2ea1e169b0be0ab6ec451b46f64bd3b
kimhyunkwang <khkhy8712@gmail.com>
2021-04-11 10:23:14 +0900

    Fix hospital_views to rendering

--- app/static/js/map.js
+++ app/static/js/map.js
@@ -5,7 +5,8 @@ var map = new naver.maps.Map('map', {
 });
 
 // 지도 위에 마커 추가
-var marker = new naver.maps.Marker({
-    position: new naver.maps.LatLng(37.5642135, 127.0016985),
-    map: map
-});
+// var marker = new naver.maps.Marker({
+//     position: new naver.maps.LatLng(37.5642135, 127.0016985),
+//     map: map
+// });
+
