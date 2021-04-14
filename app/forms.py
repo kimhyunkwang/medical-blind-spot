6d4e180da1eb31b2c248d1c062df820a9cdc3a9e
kimhyunkwang <khkhy8712@gmail.com>
2021-04-14 13:34:19 +0900

    Implement auth login, logout

--- app/forms.py
+++ app/forms.py
@@ -21,3 +21,11 @@ class RegisterForm(FlaskForm):
     repeat_password = PasswordField('repeat_password', 
                                     validators=[DataRequired('비밀번호를 다시 한 번 입력해주세요.'),
                                                 EqualTo('password', message='비밀번호가 일치하지 않습니다.')])
+
+class LoginForm(FlaskForm):
+    email = StringField('email', 
+                        validators=[DataRequired('이메일을 입력해주세요.'), 
+                                    Email('이메일 형식으로 입력해주세요.')])
+    password = PasswordField('password', 
+                            validators=[DataRequired('비밀번호를 입력해주세요.'), 
+                                        Length(min=8, max=30, message='비밀번호는 8자 이상 입력해주세요.')])
--- app/views/auth_views.py
+++ app/views/auth_views.py
@@ -3,7 +3,7 @@ from app import db
 from werkzeug.security import check_password_hash
 from werkzeug.security import generate_password_hash
 from app.models import User
-from app.forms import RegisterForm
+from app.forms import RegisterForm, LoginForm
 
 bp = Blueprint('auth', __name__, template_folder='templates', static_folder='static')
 
@@ -25,3 +25,27 @@ def register():
             return redirect(url_for('.login'))
 
     return render_template('register.html', form=form)
+
+
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    form = LoginForm()
+    
+    if request.method == 'POST' and form.validate_on_submit():  
+        user = User.query.filter(User.email == form.email.data).first()
+        
+        if user is None:
+            flash("등록되지 않은 계정입니다.", category="email_error")
+        elif not check_password_hash(user.password, form.password.data):
+            flash("비밀번호가 올바르지 않습니다.", category="pw_error")
+        else:           
+            session['user_id'] = user.id
+            return render_template('index.html')
+
+    return render_template('login.html', form=form)
+
+
+@bp.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    return render_template('index.html')
